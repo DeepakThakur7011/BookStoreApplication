ea4374a47a6447d52f552528a675625dd8f83dc6
DeepakThakurPatna <108866773+DeepakThakurPatna@users.noreply.github.com>
2025-02-27 17:06:38 +0530

    Startup File Added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
-var aee = builder.Build();
+//var builder = WebApplication.CreateBuilder(args);
+//var aee = builder.Build();
 
-aee.MapGet("/", () => "Hello World!");
+//aee.MapGet("/", () => "Hello World!");
 
-aee.Run();
+//aee.Run();
+namespace WebApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+        
+    }
+}
