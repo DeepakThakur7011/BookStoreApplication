3f9eaddb8754d933f5b19ce6918b75a8dba34d4b
DeepakThakurPatna <108866773+DeepakThakurPatna@users.noreply.github.com>
2025-03-01 18:53:41 +0530

    Model Folder added

---
+++ Models/BookModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication7.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
