d02f70f8b4b84f5f9c2a2cad9780f2449cd358e7
DeepakThakurPatna <108866773+DeepakThakurPatna@users.noreply.github.com>
2025-03-01 18:53:21 +0530

    ControllerFolder added

---
+++ Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    public class BookController : ControllerBase
+    {
+        public string name()
+        {
+            return "deepak";
+        }
+    }
+}
