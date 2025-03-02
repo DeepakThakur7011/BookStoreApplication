7cc4c866b55dc38e473c396403d78d6907701c35
DeepakThakurPatna <108866773+DeepakThakurPatna@users.noreply.github.com>
2025-03-02 15:57:00 +0530

    methodsadded

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Models;
+using WebApplication7.Repository;
 
 namespace WebApplication7.Controllers
 {
     public class BookController : ControllerBase
     {
-        public string name()
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
         {
-            return "dee";
+            _bookRepository = new BookRepository();
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+        public BookModel GetBooks(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+        public List<BookModel> SearchBooks(string bookName , string authorName)
+        {
+            return _bookRepository.SearchBook(bookName, authorName);
+               
         }
     }
 }
