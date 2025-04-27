6f8a7a9a938c2b0d0ba6e9cefce55fa1469e1757
DeepakThakur7011 <108866773+DeepakThakur7011@users.noreply.github.com>
2025-04-27 18:33:01 +0530

    all

--- obj/Debug/net8.0/WebApplication7.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed82cbf6340d4f1035ee29e5a75c8bf9c8c9e9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b25a19acda41b856a9033d0a41a13c4bc37fb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
